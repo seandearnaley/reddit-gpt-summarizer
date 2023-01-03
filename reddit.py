cfecdc2c8937028660c161cb8f34e1f9cdb67d42
Sean Dearnaley <seandearnaley@hotmail.com>
2023-01-03 16:11:15 -0500

    add type hints

--- reddit.py
+++ reddit.py
@@ -2,54 +2,65 @@
 from datetime import datetime
 import openai
 import os
-from jsonpath_ng.ext import parse
 import re
-import requests
 from dotenv import load_dotenv
-from utils import get_token_length, generate_filename
+from utils import get_token_length, generate_filename, request_json_from_url
 from pathlib import Path
+from typing import Generator, Tuple, List, Union
 
 load_dotenv()
 
 # number of tokens to summarize to
-CHUNK_SIZE = 2900
-GROUP_LIMIT = 2
+MAX_CHUNK_SIZE = 2600
+GROUP_LIMIT = 1
+
+# OpenAI Constants
+MODEL_MAX_TOKENS = 4000
+GPT_MODEL = "text-davinci-003"
 
 # reddit URL/ make sure to add .json to the end of the URL
-REDDIT_URL = 'https://www.reddit.com/r/Futurology/comments/y6od32/artists_say_ai_image_generators_are_copying_their.json'
+REDDIT_URL = "https://www.reddit.com/r/LeopardsAteMyFace/comments/101ykfm/bow_before_the_genius_god_of_business.json"
 
-PROMPT_STRING = 'Building upon this, professionally edit the article, use any additional relevant information provided in the comment thread below, revise, enhance and punctuate. Don\'t include code or commands and optimize for readability.'
+PROMPT_STRING = "Building upon this, professionally edit the article, use any additional relevant information provided in the comment thread below, revise, enhance and make it engaging. Don't include code or commands and optimize for facts as well as readability, share sentiment but remain inpartial."
 
 openai.organization = os.environ.get("OPENAI_ORG_ID")
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 
-def complete_chunk(prompt) -> str:
-    print('prompt=' + prompt)
-    print('token length: ' + str(get_token_length(prompt)))
+def complete_chunk(prompt: str) -> str:
+    """
+    Use OpenAI's GPT-3 model to complete a chunk of text based on the given prompt.
+
+    Args:
+        prompt (str): The prompt to use as the starting point for text completion.
+
+    Returns:
+        str: The completed chunk of text.
+    """
+    print("prompt=" + prompt)
+    print("token length: " + str(get_token_length(prompt)))
     response = openai.Completion.create(
-        model="text-davinci-003",
+        model=GPT_MODEL,
         prompt=prompt,
         temperature=0.9,
-        max_tokens=4000-get_token_length(prompt),
+        max_tokens=MODEL_MAX_TOKENS - get_token_length(prompt),
         # top_p=1,
         # frequency_penalty=0,
         # presence_penalty=0.6
     )
-    # print(response)
     # print(response.choices[0].text)
     return response.choices[0].text
 
 
-def concatenate_bodies(contents):
-    # Concatenate the bodies into an array of CHUNK_SIZE token strings
+def concatenate_bodies(contents: List[Tuple[str, str]]) -> List[str]:
+    # Concatenate the bodies into an array of newline delimited strings that are ~MAX_CHUNK_SIZE tokens long (NOTE: potential bug here if a single body is > MAX_CHUNK_SIZE tokens)
     results = []
     result = ""
     for t in contents:
-        if t[1].strip():
+        if t[1]:
             t = (t[0], re.sub(r"\n+", "\n", t[1]))
-            result += t[1].strip() + "\n"
-            if get_token_length(result) > CHUNK_SIZE:
+            result += t[1] + "\n"
+            if get_token_length(result) > MAX_CHUNK_SIZE:
                 results.append(result)
                 result = ""
     if result:
@@ -57,38 +68,52 @@ def concatenate_bodies(contents):
     return results
 
 
-def get_body_contents(data, path=[]):
-    # assume 'body' is the key reddit uses for the body content/text
+def get_body_contents(
+    data: Union[List[Union[dict, str]], dict], path: List[str] = []
+) -> Generator[Tuple[str, str], int, None]:
+    """
+    Generator function that yields tuples of the form (path, body_content) for
+    all dictionaries in the input data with a key of 'body'.
+    """
+    # If data is a dictionary, check if it has a 'body' key
     if isinstance(data, dict):
-        if 'body' in data:
-            path_str = '/'.join([str(x) for x in path])
-            yield (path_str, data['body'])
+        if "body" in data:
+            # If the dictionary has a 'body' key, yield the path and value of the 'body' key
+            path_str = "/".join([str(x) for x in path])
+            yield (path_str, data["body"])
+        # Iterate through the dictionary's key-value pairs
         for key, value in data.items():
+            # Recursively call the function with the value and updated path
             yield from get_body_contents(value, path + [key])
+    # If data is a list, iterate through the elements
     elif isinstance(data, list):
         for index, item in enumerate(data):
+            # Recursively call the function with the element and updated path
             yield from get_body_contents(item, path + [index])
 
 
 def main():
-    # Make a request to the URL and retrieve the JSON content
-    with requests.get(REDDIT_URL, headers={'User-Agent': 'Mozilla/5.0'}) as response:
-        data = response.json()
+    data = request_json_from_url(REDDIT_URL)
 
-    # Create a JSONPath expression to find the first item with a "title" attribute
-    title = parse('$..title').find(data)[0].value
-    selftext = parse('$..selftext').find(data)[0].value
+    # write data to output file
+    Path("output.json").write_text(str(data), encoding="utf-8")
 
-    # get an array of body contents
-    contents = list(get_body_contents(data, []))
+    # get the child with a "title" attribute
+    child_data = data[0]["data"]["children"][0]["data"]
+    title = child_data["title"]
+    selftext = child_data["selftext"]
 
+    # get an array of body contents, this will get you back and array of tuples to the path and body content
+    contents = get_body_contents(data, [])
+
+    # concatenate the bodies into an array of newline delimited strings that are ~MAX_CHUNK_SIZE tokens long
     groups = concatenate_bodies(contents)
 
     # Get the current working directory
     cwd = os.getcwd()
 
     # Define the relative path to the output folder
-    output_folder = 'outputs'
+    output_folder = "outputs"
 
     # Construct the full path to the output folder
     output_path = os.path.join(cwd, output_folder)
@@ -97,14 +122,14 @@ def main():
     os.makedirs(output_path, exist_ok=True)
 
     # Create the output filename with a timestamp
-    timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
-    output_filename = f'{generate_filename(title)}_{timestamp}.txt'
-
-    # initialize the prefix with the title of the reddit thread
-    prefix = f"Title: {title}\n{selftext}"
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    output_filename = f"{generate_filename(title)}_{timestamp}.txt"
 
     # Construct the full path to the output file
     output_file_path = os.path.join(output_path, output_filename)
+
+    # initialize the prefix with the title and selftext of the reddit thread JSON
+    prefix = f"Title: {title}\n{selftext}"
 
     # Use f-strings for string formatting
     output = f"START\n\n{PROMPT_STRING}\n\n"
@@ -128,7 +153,7 @@ Title:"""
         output += summary
 
     # Use the Path.write_text method to write to the output file
-    Path(output_file_path).write_text(output)
+    Path(output_file_path).write_text(output, encoding="utf-8")
 
     print(output)
 
--- utils.py
+++ utils.py
@@ -2,12 +2,13 @@ import requests
 from transformers import GPT2Tokenizer
 from bs4 import BeautifulSoup
 import re
+import json
 
 # Set up the tokenizer
 tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 
 # Headers to mimic a browser visit
-headers = {'User-Agent': 'Mozilla/5.0'}
+headers = {"User-Agent": "Mozilla/5.0"}
 
 
 def get_token_length(text):
@@ -22,17 +23,34 @@ def download_parse_reddit_thread(url):
     html_content = response.text
 
     # Parse the HTML content using Beautiful Soup
-    soup = BeautifulSoup(html_content, 'html.parser')
+    soup = BeautifulSoup(html_content, "html.parser")
 
     return soup.get_text("|", strip=True).replace("Reply|Share|Report|Save|Follow", "")
 
 
 def generate_filename(title) -> str:
     # Remove all special characters and spaces from the title
-    filename = re.sub(r'[^\w\s]', '', title)
+    filename = re.sub(r"[^\w\s]", "", title)
     # Replace all remaining spaces with underscores
-    filename = filename.replace(' ', '_')
+    filename = filename.replace(" ", "_")
     # Shorten the filename if it is too long
     if len(filename) > 100:
         filename = filename[:100]
     return filename
+
+
+def request_json_from_url(url: str) -> dict:
+    # Make a request to the URL and retrieve the JSON content
+    try:
+        with requests.get(url, headers={"User-Agent": "Mozilla/5.0"}) as response:
+            response.raise_for_status()
+            try:
+                data = response.json()
+            except json.decoder.JSONDecodeError as err:
+                print(f"There was an error decoding the JSON response: {err}")
+                return
+    except requests.exceptions.RequestException as err:
+        print(f"There was an error making the request: {err}")
+        return
+
+    return data
