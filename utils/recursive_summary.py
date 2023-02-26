278a66d93aae73cd592c616806fabecfae11ce87
Sean Dearnaley <seandearnaley@hotmail.com>
2023-02-26 15:17:36 -0500

    refactors

--- utils/recursive_summary.py
+++ utils/recursive_summary.py
@@ -6,11 +6,11 @@ a summary of the reddit thread.
 
 import os
 import sys
-from typing import Any, Callable, Dict
+from typing import Any, Dict
 
 import openai
 from dotenv import load_dotenv
-from ratelimit import limits, sleep_and_retry
+from pyrate_limiter import Duration, Limiter, RequestRate
 
 from utils.openai import estimate_word_count, num_tokens_from_string
 
@@ -35,18 +35,12 @@ RATE_LIMIT = 10  # max number of requests per minute
 RATE_LIMIT_PERIOD = 60  # period in seconds
 
 
-# Decorator for token bucket rate limiting
-def token_bucket_rate_limit(func: Callable[..., Any]) -> Callable[..., Any]:
-    """
-    Decorator for token bucket rate limiting.
-    """
+rate_limits = (RequestRate(10, Duration.MINUTE),)  # 10 requests a minute
 
-    @sleep_and_retry
-    @limits(calls=RATE_LIMIT, period=RATE_LIMIT_PERIOD)
-    def rate_limited_func(*args: Any, **kwargs: Any) -> Any:
-        return func(*args, **kwargs)
 
-    return rate_limited_func
+# Create the rate limiter
+# Pyrate Limiter instance
+limiter = Limiter(*rate_limits)
 
 
 def load_text_file(filename: str):
@@ -73,7 +67,43 @@ def write_text_file(text: str, filename: str):
     return file_path
 
 
-@token_bucket_rate_limit
+def recursive_summarization(
+    summary_size: int, chunk_text: str, prefix_text: str = ""
+) -> str:
+    """
+    Recursive summarization function.
+    """
+    summary_string = (
+        f"```{prefix_text}```\n\n"
+        + "new text:\n\n```"
+        # + f"{estimate_word_count(max_token_length)} words:\n"
+        + chunk_text
+        + "```\n\nsummarize then append to last text, write close to [replace] "
+        " words, use extractive summarization if you have too much text, use"
+        " abstractive summarization (no gibberish) if you don't have enough:\n\n```"
+    )
+
+    summary_string = summary_string.replace(
+        "[replace]", str(estimate_word_count(summary_size))
+    )
+
+    response: Dict[str, Any] = openai.Completion.create(  # type: ignore
+        model=GPT_MODEL,
+        prompt=summary_string,
+        frequency_penalty=0.5,
+        max_tokens=MAX_TOKENS
+        - num_tokens_from_string(summary_string),  # eg 4000-(len chunk+len string)
+    )
+
+    if len(response) == 0:
+        print("response=", response)  # error
+        return "Error: unable to generate response."
+
+    response_text = response["choices"][0]["text"]
+    return response_text
+
+
+@limiter.ratelimit("summarize_text")
 def summarize_text(
     text: str, max_token_length: int, max_tokens: int = MAX_TOKENS
 ) -> str:
@@ -85,36 +115,6 @@ def summarize_text(
 
     summary_size = max_token_length if max_token_length <= max_tokens else max_tokens
 
-    def recursive_summarization(chunk_text: str, prefix_text: str = "") -> str:
-        summary_string = (
-            f"```{prefix_text}```\n\n"
-            + "new text:\n\n```"
-            # + f"{estimate_word_count(max_token_length)} words:\n"
-            + chunk_text
-            + "```\n\nsummarize then append to last text, write close to [replace] "
-            " words, use extractive summarization if you have too much text, use"
-            " abstractive summarization (no gibberish) if you don't have enough:\n\n```"
-        )
-
-        summary_string = summary_string.replace(
-            "[replace]", str(estimate_word_count(summary_size))
-        )
-
-        response: Dict[str, Any] = openai.Completion.create(  # type: ignore
-            model=GPT_MODEL,
-            prompt=summary_string,
-            frequency_penalty=0.5,
-            max_tokens=MAX_TOKENS
-            - num_tokens_from_string(summary_string),  # eg 4000-(len chunk+len string)
-        )
-
-        if len(response) == 0:
-            print("response=", response)  # error
-            return "Error: unable to generate response."
-
-        response_text = response["choices"][0]["text"]
-        return response_text
-
     chunks = [
         text[i : i + MAX_CHUNK_LENGTH] for i in range(0, len(text), MAX_CHUNK_LENGTH)
     ]
@@ -122,7 +122,7 @@ def summarize_text(
     summary = ""
     for chunk in chunks:
         print("chunk=", chunk)
-        summary = recursive_summarization(chunk, summary)
+        summary = recursive_summarization(summary_size, chunk, summary)
         result += summary
 
     num_tokens = num_tokens_from_string(result)
